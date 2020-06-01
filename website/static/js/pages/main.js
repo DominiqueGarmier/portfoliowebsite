158c49b78ae89bee556bf3b8d08ed03e4820a1df
Dominique <dominique@garmier.ch>
2020-06-01 13:12:49 +0200

    welcome animation done

--- website/static/js/pages/main.js
+++ website/static/js/pages/main.js
@@ -1,9 +1,9 @@
-var ScrollDownPrompt = document.getElementById("scroll-down-prompt")
+var MovingBanner = document.getElementById("moving-banner")
 
-var myScrollFunc = function() {
+var BannerScrolling = function() {
     var y = window.scrollY;
-    ScrollDownPrompt.style.bottom = Math.round(y / 20).toString() + '%'
-    ScrollDownPrompt.style.opacity = Math.max(1 - y / 400, 0).toString()
+    MovingBanner.style.top = -Math.round(y / 10).toString() + '%'
+    MovingBanner.style.opacity = Math.max(1 - y / 400, 0).toString()
 }
 
-window.addEventListener("scroll", myScrollFunc)
+window.addEventListener("scroll", BannerScrolling)
